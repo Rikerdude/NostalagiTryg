da554b0c7c2065b4cb6aaf0a9564e8b2d4bd7968
Rikerdude <riker789@gmail.com>
2016-02-09 10:56:04 +0100

    start

---
+++ RepoNosTryg/DDMF/Conn.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+
+namespace RepoNosTryg
+{
+    public static class Conn
+    {
+        
+        public static SqlConnection GetCon()
+        {
+            SqlConnection con = new SqlConnection("server=SERVER_IP;database=DATABASENAME;uid=USERNAME;pwd=PASSWORD;MultipleActiveResultSets=True");
+            return con;
+        }
+
+        public static SqlConnection CreateConnection()
+        {
+            var cn = GetCon();
+            cn.Open();
+            return cn;
+        }
+    }
+}
